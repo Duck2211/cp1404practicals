b265c85c7ec59d5db671fea0401c67e9736d8511
Duck2211 <ducktrungnguyen@gmail.com>
2023-03-23 16:56:08 +0800

    Add password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -3,4 +3,4 @@ code= input("what is your password:")
 while code != PASSWORD:
     print("invalid")
     code = input("what is your password:")
-print("Welcome back duck")
+print("Welcome back sir duck")
