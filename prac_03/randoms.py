a93a040b4e61375f5bd6ddd2946757de96a99fe4
Duck2211 <ducktrungnguyen@gmail.com>
2023-04-01 17:19:18 +0800

    Practical 3

---
+++ prac_03/randoms.py
@@ -0,0 +1,11 @@
+import random
+
+
+# In line 1, the smallest number I could get was 5 and the largest was 20.
+
+# In line 2,the smallest number I could get was 3 and the largest was 9.
+# Moreover, the code can't get 4 since the starting number is 3 and the next number will get plus by 2.
+
+# in line 3,the smallest number I could get was 2.5 and the largest was 5.5.
+
+print(random.randint(1,100))
